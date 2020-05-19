0222b0e2ada5c8f95fcad05971de5c600738d86d
Ashtonn77 <ashtonrbay@gmail.com>
2020-05-19 20:27:17 +0200

    added validation

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api',{
     useNewUrlParser: true,
@@ -7,40 +8,61 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api',{
 
 const User = mongoose.model('User', {
     name:{
-        type: String
+        type: String,
+        trim: true,
+        required: true
+    },
+    email:{
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error('Invalid email')
+            }
+        }
     },
     age:{
-        type: Number
+        type: Number,
+        default:0,
+        validate(value){
+            if(value < 0){
+                throw new Error('You can\'t age backwards...you\'re not Benjamin Button')
+            }
+        }
     }
 })
 
-const Task = mongoose.model('Task', {
-    description:{
-        type: String
-    },
-    completed:{
-        type: Boolean
-    }
-})
 
 const me = new User({
-    name:'Ashton',
-    age: 'Mikes'
+   name: 'Ashton    ',
+   email: 'ASHTONRBAY@GMAIL.com   '
 })
 
-const todo = new Task({
-    description: 'Study more',
-    completed: true
+
+me.save().then(() => {
+    console.log(me)
+}).catch((error) => {
+    console.log(error.message)
 })
 
-// me.save().then(() => {
-//     console.log(me)
+// const Task = mongoose.model('Task', {
+//     description:{
+//         type: String
+//     },
+//     completed:{
+//         type: Boolean
+//     }
+// })
+
+// const todo = new Task({
+//     description: 'Study more',
+//     completed: true
+// })
+
+// todo.save().then(() => {
+//     console.log(todo)
 // }).catch((error) => {
 //     console.log(error)
 // })
-
-todo.save().then(() => {
-    console.log(todo)
-}).catch((error) => {
-    console.log(error)
-})
