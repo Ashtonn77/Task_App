af4226164e2f881ff5a6948526eeff20b87304c2
Ashtonn77 <ashtonrbay@gmail.com>
2020-06-02 19:52:59 +0200

    added login verification

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -15,6 +15,20 @@ router.post("/users", async (req, res) => {
   }
 });
 
+//verify details
+router.post("/users/login", async (req, res) => {
+  try {
+    const user = await User.findByCredentials(
+      req.body.email,
+      req.body.password
+    );
+
+    res.send(user);
+  } catch (e) {
+    res.status(400).send();
+  }
+});
+
 //read
 router.get("/users", async (req, res) => {
   try {
